57f87006ba58ef5d509f83d1d1e86f24321b9ddd
you <582838918@qq.com>
2019-09-18 17:51:49 +0800

    优化心愿单相关api代码健壮，增加分页

--- membership/controllers/member_desire.py
+++ membership/controllers/member_desire.py
@@ -14,30 +14,49 @@ from odoo.addons.restful.controllers.main import validate_token
 from .base import BaseController
 from .base import _ocean_platform_to_partner
 import logging
+import functools
 _logger = logging.getLogger(__name__)
+
+def  check_personal_company(func):
+	"""
+	验证必传参数
+	:param func:
+	:return:
+	"""
+	@functools.wraps(func)
+	def wrap(self, *args, **kwargs):
+		personal_platform_id = kwargs.get('personal_platform_id', False)
+		company_platform_id = kwargs.get('company_platform_id', False)
+		if not personal_platform_id or not company_platform_id:
+			return BaseController().res_err(600)
+		company_id = _ocean_platform_to_partner(company_platform_id)
+		personal_id = _ocean_platform_to_partner(personal_platform_id)
+		if not company_id or not personal_id:
+			return BaseController().res_err(604)
+		kwargs['company_id']= company_id
+		kwargs['personal_id']= personal_id
+		return func(self, *args, **kwargs)
+	return wrap
 
 class MemberDesireController(http.Controller,BaseController):
 
 	#添加愿望
+	@check_personal_company
 	@http.route("/membership/desire/add",type='http',auth='none',csrf=False,methods=['POST'])
 	def member_desire_add(self,**kwargs):
-		personal_platform_id = kwargs.get('personal_platform_id', False)
-		company_platform_id = kwargs.get('company_platform_id', False)
+		company_id = kwargs.get('company_id')
+		personal_id = kwargs.get('personal_id')
 		service_id = kwargs.get('service_id', False)
 		seller_id = kwargs.get('seller_id',False)
-		if not personal_platform_id or not company_platform_id or not service_id:
+		if  not service_id:
 			return self.res_err(600)
 		if not seller_id:
 			return self.res_err(600,data="请传入供应商id")
-		if not service_id.isdigit():
+		if not service_id.isdigit() or not seller_id.isdigit():
 			return self.res_err(603)
-		company_id = _ocean_platform_to_partner(company_platform_id)
-		personal_id = _ocean_platform_to_partner(personal_platform_id)
-		if not company_id or not personal_id:
-			return self.res_err(604)
 		services = request.env['hotel.services'].sudo().search([("id","=",int(service_id))])
 		if not services:
-			return self.res_err(600,data="该服务不存在")
+			return self.res_err(600,data=u"该服务不存在")
 		#服务id
 		desire_id = services[0].id
 		product_tmpl_id = services[0].product_id.product_tmpl_id.id
@@ -57,7 +76,6 @@ class MemberDesireController(http.Controller,BaseController):
 			_logger.error("http>>/membership/desire/add,create membership.desire: %s"%e)
 			return self.res_err(-1)
 		else:
-			# date = desires
 			data = {
 				"desire_order": desires.desire_order
 			}
@@ -77,34 +95,31 @@ class MemberDesireController(http.Controller,BaseController):
 		return _dict
 
 	#愿望列表查询
+	@check_personal_company
 	@http.route("/membership/desire/query/list", type='http', auth='none', csrf=False, methods=['GET'])
 	def member_desire_query_list(self, **kwargs):
-		personal_platform_id = kwargs.get('personal_platform_id', False)
-		company_platform_id = kwargs.get('company_platform_id', False)
+		company_id = kwargs.get('company_id')
+		personal_id = kwargs.get('personal_id')
 		page = kwargs.get('page',1) #分页用
 		limit = kwargs.get('limit',20)
-		if not personal_platform_id or not company_platform_id:
-			return self.res_err(600)
-		company_id = _ocean_platform_to_partner(company_platform_id)
-		personal_id = _ocean_platform_to_partner(personal_platform_id)
 		if not company_id or not personal_id:
 			return self.res_err(604)
 		domain = [("partner_id","=",personal_id),("company_id","=",company_id)]
-		desires = request.env['membership.desire'].sudo().search(domain)
+		if not page.isdigit() or not limit.isdigit():
+			return self.res_err(603)
+		page=int(page)
+		limit=int(limit)
+		offset=(page-1)*limit
+		desires = request.env['membership.desire'].sudo().search(domain,limit=limit,offset=offset)
+		count = request.env['membership.desire'].sudo().search_count(domain)
 		data = [self._handle_desire_list_dict(desire) for desire in desires]
-		return self.res_ok(count=len(data),data=data)
+		return self.res_ok(count=count,data=data)
 
-
+	@check_personal_company
 	@http.route("/membership/desire/delete", type='http', auth='none', csrf=False, methods=['POST'])
 	def member_desire_delete(self,**kwargs):
-		personal_platform_id = kwargs.get('personal_platform_id', False)
-		company_platform_id = kwargs.get('company_platform_id', False)
 		desire_ids = kwargs.get('desire_ids',False)
-
-		print(desire_ids,type(desire_ids))
-		print(ast.literal_eval(desire_ids),type(ast.literal_eval(desire_ids)))
-		req = re.match(r'[\d\[\],]+$',desire_ids)
-		# print(tt)
+		req = re.match(r'^\[[\d,]\]$',str(desire_ids))
 		if not req:
 			return self.res_err(603)
 		try:
